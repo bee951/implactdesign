518498140a6f640d87ce4fc9e937884333fa2acb
Ben Easterwood <ben.easterwood@gmail.com>
2018-07-17 23:48:17 -0500

    one image left to add

---
+++ script.js
@@ -0,0 +1,6 @@
+$(function(){
+    $("#abtlink").attr("href","#about")
+    $("#explink").attr("href","#experience")
+    $("#worklink").attr("href","#work")
+    $("#contlink").attr("href","#contact")
+})
